c52ac9ce2f40c333a678193d8a87def893373ec9
UnsignedByte <edlam1889@gmail.com>
2021-10-12 19:46:48 -0700

    hello world

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+/*
+* @Author: UnsignedByte
+* @Date:   2021-10-12 19:25:48
+* @Last Modified by:   UnsignedByte
+* @Last Modified time: 2021-10-12 19:43:09
+*/
+
+#include <iostream>
+
+int main() {
+	std::cout << "HI" << std::endl;
+}
